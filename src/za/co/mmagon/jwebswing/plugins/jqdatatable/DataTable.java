44f40686af5fcfbb55ab02548489388ac63cc49b
Marc Magon <ged_marc@hotmail.com>
2017-09-29 18:41:00 +0200

    bower update to latest version, naming update, lots on data tables, include the bs4 and rowreorder plugins
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

---
+++ src/za/co/mmagon/jwebswing/plugins/jqdatatable/DataTable.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package za.co.mmagon.jwebswing.plugins.jqdatatable;
+
+import za.co.mmagon.jwebswing.base.html.*;
+import za.co.mmagon.jwebswing.base.html.attributes.TableAttributes;
+import za.co.mmagon.jwebswing.base.html.interfaces.GlobalChildren;
+import za.co.mmagon.jwebswing.plugins.ComponentInformation;
+
+/**
+ * The JWDataTable implementation
+ *
+ * @param <T> The table row type this table reflects
+ * @param <J>
+ *
+ * @author MMagon
+ * @version 1.0
+ * @since 2014 09 30
+ */
+@ComponentInformation(name = "Data Tables",
+		description = "The core data tables component",
+		url = "https://www.datatables.net/")
+public class DataTable<T extends TableRow, J extends DataTable<T, J>> extends Table<J> implements GlobalChildren
+{
+	
+	private static final long serialVersionUID = 1L;
+	/**
+	 * The associated feature for the data table
+	 */
+	private DataTableFeature feature;
+	/**
+	 * The header grouping for a data table
+	 */
+	private TableHeaderGroup headerGroup;
+	/**
+	 * The footer grouping for a data table
+	 */
+	private TableFooterGroup footerGroup;
+	/**
+	 * The table grouping for a data table
+	 */
+	private TableBodyGroup bodyGroup;
+	/**
+	 * The caption item for a table
+	 */
+	private TableCaption captionOfTable;
+	/**
+	 * If the dynamic features are enabled on this data table
+	 */
+	private boolean enableDynamicFeature;
+	
+	/**
+	 * Construct a new interactive table that is theme compatible, with cell spacing and padding as 0. Sets dynamic feature to disabled
+	 * <p>
+	 *
+	 * @param headerGroup The table header group creating for
+	 */
+	public DataTable(TableHeaderGroup headerGroup)
+	{
+		this(headerGroup, true);
+	}
+	
+	/**
+	 * Construct a new interactive table that is theme compatible, with cell spacing and padding as 0.
+	 * <p>
+	 *
+	 * @param headerGroup          The table header group creating for
+	 * @param enableDynamicFeature Enables the DataTable Feature
+	 */
+	public DataTable(TableHeaderGroup headerGroup, boolean enableDynamicFeature)
+	{
+		this.enableDynamicFeature = enableDynamicFeature;
+		addAttribute(TableAttributes.CellSpacing, 0);
+		addAttribute(TableAttributes.CellPadding, 0);
+		this.headerGroup = headerGroup;
+	}
+	
+	@Override
+	public void init()
+	{
+		if (!isInitialized())
+		{
+			if (isEnableDynamicFeature())
+			{
+				addFeature(getFeature());
+			}
+		}
+		super.init();
+	}
+	
+	@Override
+	public DataTableOptions getOptions()
+	{
+		return getFeature().getOptions();
+	}
+	
+	public final DataTableFeature getFeature()
+	{
+		if (feature == null)
+		{
+			feature = new DataTableFeature(this);
+		}
+		return feature;
+	}
+	
+	/**
+	 * Gets the header group for this Data Table
+	 *
+	 * @return
+	 */
+	public TableHeaderGroup getHeaderGroup()
+	{
+		if (headerGroup == null)
+		{
+			setHeaderGroup(new TableHeaderGroup());
+		}
+		return headerGroup;
+	}
+	
+	/**
+	 * Sets the header group for this table
+	 * <p>
+	 *
+	 * @param headerGroup
+	 */
+	public J setHeaderGroup(TableHeaderGroup headerGroup)
+	{
+		getChildren().remove(this.headerGroup);
+		this.headerGroup = headerGroup;
+		if (!getChildren().contains(headerGroup))
+		{
+			add(headerGroup);
+		}
+		
+		return (J) this;
+	}
+	
+	/**
+	 * Gets the footer group for this data table
+	 *
+	 * @return
+	 */
+	public TableFooterGroup getFooterGroup()
+	{
+		
+		if (footerGroup == null)
+		{
+			setFooterGroup(new TableFooterGroup());
+		}
+		return footerGroup;
+	}
+	
+	/**
+	 * sets the footer group for this table
+	 * <p>
+	 *
+	 * @param footerGroup
+	 */
+	public J setFooterGroup(TableFooterGroup footerGroup)
+	{
+		getChildren().remove(this.footerGroup);
+		this.footerGroup = footerGroup;
+		if (!getChildren().contains(footerGroup))
+		{
+			add(footerGroup);
+		}
+		return (J) this;
+	}
+	
+	/**
+	 * Gets the body group for this data table
+	 *
+	 * @return
+	 */
+	public TableBodyGroup getBodyGroup()
+	{
+		if (bodyGroup == null)
+		{
+			setBodyGroup(new TableBodyGroup());
+		}
+		return bodyGroup;
+	}
+	
+	/**
+	 * Sets the body group for the table
+	 * <p>
+	 *
+	 * @param bodyGroup
+	 */
+	public J setBodyGroup(TableBodyGroup bodyGroup)
+	{
+		getChildren().remove(this.bodyGroup);
+		this.bodyGroup = bodyGroup;
+		if (!getChildren().contains(bodyGroup))
+		{
+			add(bodyGroup);
+		}
+		return (J) this;
+	}
+	
+	/**
+	 * Returns the Table Caption associated with this object plus positioning utilities
+	 *
+	 * @return
+	 */
+	public TableCaption getCaptionOfTable()
+	{
+		if (captionOfTable == null)
+		{
+			setCaptionOfTable(new TableCaption(null));
+		}
+		return captionOfTable;
+	}
+	
+	/**
+	 * Sets the caption for the table
+	 *
+	 * @param captionOfTable
+	 */
+	public J setCaptionOfTable(TableCaption captionOfTable)
+	{
+		this.captionOfTable = captionOfTable;
+		return (J) this;
+	}
+	
+	/**
+	 * If dynamic features are enabled
+	 *
+	 * @return
+	 */
+	public boolean isEnableDynamicFeature()
+	{
+		return enableDynamicFeature;
+	}
+	
+	/**
+	 * Sets if the dynamic features of this table must be rendered
+	 *
+	 * @param enableDynamicFeature
+	 *
+	 * @return
+	 */
+	public J setEnableDynamicFeature(boolean enableDynamicFeature)
+	{
+		this.enableDynamicFeature = enableDynamicFeature;
+		return (J) this;
+	}
+}
---
+++ src/za/co/mmagon/jwebswing/plugins/jqdatatable/DataTableOptions.java
@@ -0,0 +1,413 @@
+/* 
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package za.co.mmagon.jwebswing.plugins.jqdatatable;
+
+import za.co.mmagon.jwebswing.htmlbuilder.javascript.JavaScriptPart;
+
+/**
+ * The Options for the Data Table
+ * <p>
+ *
+ * @author GedMarc
+ * @since 09 May 2015
+ */
+public class DataTableOptions<J extends DataTableOptions<J>> extends JavaScriptPart<J>
+{
+	
+	private static final long serialVersionUID = 1L;
+	/**
+	 * the document object model layout
+	 */
+	private String dom;
+	/**
+	 * Add or modify data submitted to the server upon an Ajax request
+	 */
+	private String ajax_data;
+	/**
+	 * Data property or manipulation method for table data
+	 */
+	private String ajax_dataSrc;
+	/**
+	 * Load data for the table's content from an Ajax source
+	 */
+	private String ajax;
+	/**
+	 * Data to use as the display data for the table.
+	 */
+	private String data;
+	/**
+	 * Feature control DataTables' smart column width handling
+	 */
+	private Boolean autoWidth;
+	/**
+	 * Feature control deferred rendering for additional speed of initialization.
+	 */
+	private Boolean deferRender;
+	/**
+	 * Feature control table information display field
+	 */
+	private Boolean info;
+	/**
+	 * Use markup and classes for the table to be themed by jQuery UI ThemeRoller.
+	 *
+	 * @deprecated
+	 */
+	private Boolean jQueryUI;
+	/**
+	 * Feature control the end user's ability to change the paging display length of the table.
+	 */
+	private Boolean lengthChange;
+	/**
+	 * Feature control ordering (sorting) abilities in DataTables.
+	 */
+	private Boolean ordering;
+	/**
+	 * Enable or disable table pagination.
+	 */
+	private Boolean paging;
+	/**
+	 * Feature control the processing indicator.
+	 */
+	private Boolean processing;
+	/**
+	 * Horizontal scrolling
+	 */
+	private Boolean scrollX;
+	/**
+	 * Horizontal scrolling
+	 */
+	private Integer scrollXNum;
+	/**
+	 * Vertical scrolling
+	 */
+	private Boolean scrollY;
+	/**
+	 * Vertical scrolling
+	 */
+	private Integer scrollYNum;
+	/**
+	 * Feature control search (filtering) abilities
+	 */
+	private Boolean searching;
+	/**
+	 * Feature control DataTables' server-side processing mode.
+	 */
+	private Boolean serverSide;
+	/**
+	 * State saving - restore table state on page reload
+	 */
+	private Boolean stateSave;
+	/**
+	 * State saving - restore table state on page reload
+	 */
+	private Boolean retrieve = true;
+	/**
+	 * If the data table should be destroyed
+	 */
+	private Boolean destroy;
+	
+	public DataTableOptions()
+	{
+	
+	}
+	
+	public Boolean getAutoWidth()
+	{
+		return autoWidth;
+	}
+	
+	public J setAutoWidth(Boolean autoWidth)
+	{
+		this.autoWidth = autoWidth;
+		return (J)this;
+	}
+	
+	public Boolean getDeferRender()
+	{
+		return deferRender;
+	}
+	
+	public J setDeferRender(Boolean deferRender)
+	{
+		this.deferRender = deferRender;
+		return (J)this;
+	}
+	
+	public Boolean getInfo()
+	{
+		return info;
+	}
+	
+	public J setInfo(Boolean info)
+	{
+		this.info = info;
+		return (J)this;
+	}
+	
+	/**
+	 * @return
+	 *
+	 * @deprecated
+	 */
+	public Boolean getjQueryUI()
+	{
+		return jQueryUI;
+	}
+	
+	/**
+	 * @param jQueryUI
+	 *
+	 * @deprecated
+	 */
+	public J setjQueryUI(Boolean jQueryUI)
+	{
+		this.jQueryUI = jQueryUI;
+		return (J)this;
+	}
+	
+	public Boolean getLengthChange()
+	{
+		return lengthChange;
+	}
+	
+	public J setLengthChange(Boolean lengthChange)
+	{
+		this.lengthChange = lengthChange;
+		return (J)this;
+	}
+	
+	public Boolean getOrdering()
+	{
+		return ordering;
+	}
+	
+	public J setOrdering(Boolean ordering)
+	{
+		this.ordering = ordering;
+		return (J)this;
+	}
+	
+	public Boolean getPaging()
+	{
+		return paging;
+	}
+	
+	public J setPaging(Boolean paging)
+	{
+		this.paging = paging;
+		return (J)this;
+	}
+	
+	public Boolean getProcessing()
+	{
+		return processing;
+	}
+	
+	public J setProcessing(Boolean processing)
+	{
+		this.processing = processing;
+		return (J)this;
+	}
+	
+	public Boolean getScrollX()
+	{
+		return scrollX;
+	}
+	
+	public J setScrollX(Boolean scrollX)
+	{
+		this.scrollX = scrollX;
+		return (J)this;
+	}
+	
+	public Integer getScrollXNum()
+	{
+		return scrollXNum;
+	}
+	
+	public J setScrollXNum(Integer scrollXNum)
+	{
+		this.scrollXNum = scrollXNum;
+		return (J)this;
+	}
+	
+	public Boolean getScrollY()
+	{
+		return scrollY;
+	}
+	
+	public J setScrollY(Boolean scrollY)
+	{
+		this.scrollY = scrollY;
+		return (J)this;
+	}
+	
+	public Integer getScrollYNum()
+	{
+		return scrollYNum;
+	}
+	
+	public J setScrollYNum(Integer scrollYNum)
+	{
+		this.scrollYNum = scrollYNum;
+		return (J)this;
+	}
+	
+	public Boolean getSearching()
+	{
+		return searching;
+	}
+	
+	public J setSearching(Boolean searching)
+	{
+		this.searching = searching;
+		return (J)this;
+	}
+	
+	public Boolean getServerSide()
+	{
+		return serverSide;
+	}
+	
+	public J setServerSide(Boolean serverSide)
+	{
+		this.serverSide = serverSide;
+		return (J)this;
+	}
+	
+	public Boolean getStateSave()
+	{
+		return stateSave;
+	}
+	
+	public J setStateSave(Boolean stateSave)
+	{
+		this.stateSave = stateSave;
+		return (J)this;
+	}
+	
+	public String getAjax_data()
+	{
+		return ajax_data;
+	}
+	
+	public J setAjax_data(String ajax_data)
+	{
+		this.ajax_data = ajax_data;
+		return (J)this;
+	}
+	
+	public String getAjax_dataSrc()
+	{
+		return ajax_dataSrc;
+	}
+	
+	public J setAjax_dataSrc(String ajax_dataSrc)
+	{
+		this.ajax_dataSrc = ajax_dataSrc;
+		return (J)this;
+	}
+	
+	public String getAjax()
+	{
+		return ajax;
+	}
+	
+	public J setAjax(String ajax)
+	{
+		this.ajax = ajax;
+		return (J)this;
+	}
+	
+	public String getData()
+	{
+		return data;
+	}
+	
+	public J setData(String data)
+	{
+		this.data = data;
+		return (J)this;
+	}
+	
+	public Boolean getRetrieve()
+	{
+		return retrieve;
+	}
+	
+	public J setRetrieve(Boolean retrieve)
+	{
+		this.retrieve = retrieve;
+		return (J)this;
+	}
+	
+	/**
+	 * If the data table should be destroyed
+	 * @return
+	 */
+	public Boolean getDestroy()
+	{
+		return destroy;
+	}
+	
+	/**
+	 * If the data table should be destroyed
+	 *
+	 * @param destroy
+	 *
+	 * @return
+	 */
+	public J setDestroy(Boolean destroy)
+	{
+		this.destroy = destroy;
+		return (J)this;
+	}
+	
+	/**
+	 * <p>
+	 * As is described by the basic DOM positioning example you can use the dom initialisation parameter to move DataTables features around the table to where you want them.
+	 * <p>
+	 * In addition to this, you can also use dom to create multiple instances of these table controls. Simply include the feature's identification letter where you want it to appear, as many times as
+	 * you wish, and the controls will all sync up (note that obviously the table (t) should be included only once).
+	 * <p>
+	 * This is shown in the demo below where for four key build-in features are duplicated above and below the table.
+	 *
+	 * @return
+	 */
+	public String getDom()
+	{
+		return dom;
+	}
+	
+	/**
+	 * <p>
+	 * As is described by the basic DOM positioning example you can use the dom initialisation parameter to move DataTables features around the table to where you want them.
+	 * <p>
+	 * In addition to this, you can also use dom to create multiple instances of these table controls. Simply include the feature's identification letter where you want it to appear, as many times as
+	 * you wish, and the controls will all sync up (note that obviously the table (t) should be included only once).
+	 * <p>
+	 * This is shown in the demo below where for four key build-in features are duplicated above and below the table.
+	 *
+	 * @param dom
+	 */
+	public J setDom(String dom)
+	{
+		this.dom = dom;
+		return (J)this;
+	}
+
+}
