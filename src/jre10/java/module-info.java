f2ca13033681ca1f1d94f15b4149c9fab244edda
GedMarc <ged_marc@hotmail.com>
2018-08-18 19:58:09 +0200

    Data Tables and BlueImp Updates

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,8 +1,14 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedservlets.services.IGuiceSiteBinder;
 import com.jwebmp.plugins.datatable.DataTablePageConfigurator;
+import com.jwebmp.plugins.datatable.DataTablesSiteBinder;
 
 module com.jwebmp.plugins.datatable {
 	exports com.jwebmp.plugins.datatable;
+	exports com.jwebmp.plugins.datatable.options;
+	exports com.jwebmp.plugins.datatable.enumerations;
+	exports com.jwebmp.plugins.datatable.events;
+	exports com.jwebmp.plugins.datatable.search;
 
 	requires com.jwebmp.core;
 	requires com.jwebmp.logmaster;
@@ -15,12 +21,16 @@ module com.jwebmp.plugins.datatable {
 	requires javax.servlet.api;
 	requires java.logging;
 	requires com.google.common;
+	requires com.jwebmp.guicedservlets;
+	requires org.apache.commons.lang3;
 
 	provides IPageConfigurator with DataTablePageConfigurator;
+	provides IGuiceSiteBinder with DataTablesSiteBinder;
 
-	opens com.jwebmp.plugins.datatable to com.fasterxml.jackson.databind, com.jwebmp.core;
-	opens com.jwebmp.plugins.datatable.enumerations to com.fasterxml.jackson.databind, com.jwebmp.core;
-	opens com.jwebmp.plugins.datatable.events to com.fasterxml.jackson.databind, com.jwebmp.core;
-	opens com.jwebmp.plugins.datatable.options to com.fasterxml.jackson.databind, com.jwebmp.core;
-	opens com.jwebmp.plugins.datatable.search to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.datatable to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
+	opens com.jwebmp.plugins.datatable.enumerations to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
+	opens com.jwebmp.plugins.datatable.events to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
+	opens com.jwebmp.plugins.datatable.options to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
+	opens com.jwebmp.plugins.datatable.search to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
+
 }
---
+++ src/main/java/com/jwebmp/plugins/datatable/DataTablesSiteBinder.java
@@ -0,0 +1,22 @@
+package com.jwebmp.plugins.datatable;
+
+import com.jwebmp.guicedservlets.GuiceSiteInjectorModule;
+import com.jwebmp.guicedservlets.services.IGuiceSiteBinder;
+import com.jwebmp.logger.LogFactory;
+
+import java.util.logging.Logger;
+
+public class DataTablesSiteBinder
+		implements IGuiceSiteBinder<GuiceSiteInjectorModule>
+{
+	private static final Logger log = LogFactory.getLog("DataTablesSiteBinder");
+
+	@Override
+	public void onBind(GuiceSiteInjectorModule module)
+	{
+		DataTablesSiteBinder.log.info("Serving Data Tables Data at /jwdatatables");
+		module.serveRegex$("/jwdatatables")
+		      .with(DataTablesServlet.class);
+
+	}
+}
